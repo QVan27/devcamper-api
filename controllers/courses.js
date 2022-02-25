1f8bca1ee8ac5664b28be1e385d7c7226c49ccf1
QVan27 <qvannarath@gmail.com>
2022-02-25 13:30:17 +0100

    :sparkles: feat: add Update & Delete Course

--- controllers/courses.js
+++ controllers/courses.js
@@ -72,3 +72,48 @@ exports.addCourse = asyncHandler(async (req, res, next) => {
     data: course,
   });
 });
+
+// @desc    Update course
+// @route   POST /api/v1/courses/:id
+// @access  Public
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  let course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`),
+      404
+    );
+  }
+
+  course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    success: true,
+    data: course,
+  });
+});
+
+// @desc    Delete course
+// @route   DELETE /api/v1/courses/:id
+// @access  Public
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`),
+      404
+    );
+  }
+
+  await course.remove();
+
+  res.status(200).json({
+    success: true,
+    data: {},
+  });
+});

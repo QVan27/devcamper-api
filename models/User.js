5a4a96f87cc71ddd538edecb8119e898cf96e1ee
QVan27 <qvannarath@gmail.com>
2022-02-25 15:41:54 +0100

    :sparkles: feat: add User Register & Encrypting Passwords

--- models/User.js
+++ models/User.js
@@ -1,8 +1,8 @@
-const crypto = require('crypto');
+// const crypto = require('crypto');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const randomize = require('randomatic');
+// const jwt = require('jsonwebtoken');
+// const randomize = require('randomatic');
 
 const UserSchema = new mongoose.Schema({
   name: {
@@ -46,6 +46,16 @@ const UserSchema = new mongoose.Schema({
     type: Date,
     default: Date.now,
   },
+});
+
+// Encrypt password using bcrypt
+UserSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
 });
 
 module.exports = mongoose.model('User', UserSchema);
